5deda9c2cdffa1ee2aab9ac1f7b959d95beb3433
Valentin Sack <sackvalentin@gmail.com>
2026-04-27 22:48:02 +0200

    sure

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,20 @@
 /*   By: vsack <vsack@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 20:27:31 by vsack             #+#    #+#             */
-/*   Updated: 2026/04/27 20:37:20 by vsack            ###   ########.fr       */
+/*   Updated: 2026/04/27 22:45:58 by vsack            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*read_and_stash(int fd, char *stash, char *buffer)
+static char	*read_and_stash(int fd, char *stash)
 {
-	char	*buf_local;
+	char	*buffer;
 	ssize_t	bytes_read;
 
 	bytes_read = 1;
-	buf_local = malloc(BUFFER_SIZE + 1);
-	if (!buf_local)
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
 		return (NULL);
 	while ((ft_strchr(stash, '\n') == NULL) && (bytes_read != 0))
 	{
@@ -27,8 +27,79 @@ static char	*read_and_stash(int fd, char *stash, char *buffer)
 		if (bytes_read == -1)
 		{
 			free(stash);
-			free(buf_local);
+			free(buffer);
 			return (NULL);
 		}
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin(stash, buffer);
 	}
+	free(buffer);
+	return (stash);
+}
+static char	*extract_line(char *stash)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!stash || !stash[0])
+		return (NULL);
+	while (stash[i] != '\n' && stash[i])
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*update_stash(char *stash)
+{
+	int		i;
+	char	*newstash;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	newstash = ft_substr(stash, i + 1, ft_strlen(stash) - i);
+	free(stash);
+	if (!newstash)
+		return (NULL);
+	if (!*newstash)
+	{
+		free(newstash);
+		return (NULL);
+	}
+	return (newstash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char *stash;
+	char *line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_and_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = update_stash(stash);
+	return (line);
 }
